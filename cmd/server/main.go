cf0f8e32f48e0c6fc33ea9e620bb797d16a71733
baravader <russkin.serge@gmail.com>
2025-11-07 11:43:59 +0300

    add path param in handler

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -17,7 +17,9 @@ const listeningPort = ":3000"
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/{city}", func(w http.ResponseWriter, r *http.Request) {
+		city := chi.URLParam(r, "city")
+		fmt.Println(city)
 		_, err := w.Write([]byte("welcome"))
 		if err != nil {
 			log.Println(err)
@@ -59,7 +61,7 @@ func initJobs(scheduler gocron.Scheduler) ([]gocron.Job, error) {
 
 	j, err := scheduler.NewJob(
 		gocron.DurationJob(
-			1*time.Second,
+			10*time.Second,
 		),
 		gocron.NewTask(
 			func() {
